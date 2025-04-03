f2ee5aaf32caf1a33b47e01ed58a6fa30eb78c2e
gui7sampaio <gui7sampaio@gmail.com>
2025-04-03 04:00:36 +0000

    segundo commit do java script

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 let conta = 0
 let pessoas = 0
+let porcentagem = 0
 
 const contaInput = document.querySelector("#conta")
 contaInput.addEventListener("input", receberValorConta )
 
 function receberValorConta(evento){
-   conta = Number(event.target.value)
+   conta = Number(evento.target.value)
    console.log(conta)
 }
 
@@ -24,4 +25,13 @@ function receberQuantidadePessoas(evento){
         divErro.setAttribute("id", "")
         pessoas = Number(evento.target.value)
     }
+}
+
+const botoesGorjeta = document.querySelectorAll(".gorjeta input[type='button']");
+botoesGorjeta.forEach(botao => {
+    botao.addEventListener("click", receberPorcentagem)
+})
+
+function receberPorcentagem(evento){
+    console.log(evento.target.value)
 }
